c9063d89c63de074743488bee68479b7262c38cb
JLF <32518279+JoseGnRLuis@users.noreply.github.com>
2020-11-12 18:33:27 -0300

    JLF

---
+++ Clase-02/js/index.js
@@ -0,0 +1,22 @@
+/*
+  José Luis Friedrich
+*/
+
+const name = prompt('Ingrese nombre\n33\nn: ');
+const age = Number(prompt('Ingrese edad: '));
+const ageAppearance = age - 2;
+
+//template literal concat
+alert(`Tu nombre es ${name} y si bien tenes ${age} años aunque aparentas de ${ageAppearance}.`);
+
+//Extra
+if (name === 'paula') {
+  const favouriteStudent = prompt('Ingrese nombre de su alumno favorito: ');
+
+  //normal concat
+  let result = 'El alumno favorito de ' + name + ' es ' + favouriteStudent + '. ';
+  result += favouriteStudent === 'josé luis' ? 'Gracias, lo sabía!' : 'Todo mal con vos!';
+  alert(result);
+}
+
+console.log('hasta la vista!');
