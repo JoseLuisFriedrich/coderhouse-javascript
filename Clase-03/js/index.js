0ccf3c20da293ff5972ee6b65e8837a993bba21a
JLF <32518279+JoseLuisFriedrich@users.noreply.github.com>
2020-11-18 17:02:16 -0300

    JLF

--- Clase-03/js/index.js
+++ Clase-03/js/index.js
@@ -1,3 +1,22 @@
 /*
   José Luis Friedrich
 */
+
+const showOddDaysAndSundays = () => {
+  const days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábados', 'Domingos']
+
+  for (let i = 1; i < days.length + 1; i++) {
+    //isOdd
+    if (i % 2 === 0) {
+      console.log(`Los ${days[i - 1]} (día ${i} de la semana) es par`)
+    }
+
+    //isSunday
+    if (i === 7) {
+      console.log('Sunday Morning! https://www.youtube.com/watch?v=-pob1sW4AjY')
+    }
+  }
+}
+
+//Run fn
+showOddDaysAndSundays()
