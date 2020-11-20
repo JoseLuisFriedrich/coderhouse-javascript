0e153ef756999a2be2cb474898e34aee3c642c77
JLF <32518279+JoseLuisFriedrich@users.noreply.github.com>
2020-11-20 09:56:34 -0300

    JLF

--- Clase-03/js/index.js
+++ Clase-03/js/index.js
@@ -2,21 +2,25 @@
   José Luis Friedrich
 */
 
-const showOddDaysAndSundays = () => {
-  const days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábados', 'Domingos']
+const showOddDaysAndSundays = (showPersonalizedSundayMessage) => {
+  const days = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo']
 
   for (let i = 1; i < days.length + 1; i++) {
-    //isOdd
+    //day is odd
     if (i % 2 === 0) {
-      console.log(`Los ${days[i - 1]} (día ${i} de la semana) es par`)
+      console.log(`El día ${days[i - 1]} (día ${i} de la semana) es par`)
     }
 
-    //isSunday
-    if (i === 7) {
-      console.log('Sunday Morning! https://www.youtube.com/watch?v=-pob1sW4AjY')
+    //day is Sunday
+    if (i === 7 && showPersonalizedSundayMessage) {
+      if (new Date().getHours() < 13) {
+        console.warn('Sunday Morning! https://www.youtube.com/watch?v=-pob1sW4AjY')
+      } else {
+        console.warn('Sunday Bloody Sunday! https://www.youtube.com/watch?v=EM4vblG6BVQ')
+      }
     }
   }
 }
 
-//Run fn
-showOddDaysAndSundays()
+//Run fn with personalized Sunday message
+showOddDaysAndSundays(true)
