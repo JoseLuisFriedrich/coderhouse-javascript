408c8108c7234ba5eaf26ef184b840e1c0ae43f8
JLF <32518279+JoseLuisFriedrich@users.noreply.github.com>
2020-12-21 13:12:04 -0300

    JLF

--- Class-10/js/data.js
+++ Class-10/js/data.js
@@ -19,19 +19,9 @@ const addComponent = (component, parentId) => {
     const parent = all.filter(c => c.id === parentId)[0]
     const parentLastChild = parent.last(true)
 
-    if (parentLastChild) {
-      const domParent = get(`#${parentLastChild.id}`)
-      domParent.parentNode.insertBefore(component.renderProjectComponent(), domParent.nextSibling);
-    } else {
-      const domParent = get(`#${parentId}`)
-      domParent.parentNode.insertBefore(component.renderProjectComponent(), domParent.nextSibling);
-    }
-
+    const domParent = get(parentLastChild ? `#${parentLastChild.id}` : `#${parentId}`);
+    domParent.parentNode.insertBefore(component.renderProjectComponent(), domParent.nextSibling);
     parent.appendChild(component)
-
-    //referenceNode
-    //get(`#${parentId}`).appendChild(component.renderProjectComponent())
-    //console.log()
   }
 
   // console.log(`Added ${component.id} in parent ${parentId}`)
@@ -43,25 +33,9 @@ const addComponent = (component, parentId) => {
   return component
 }
 
-// const search = (value) => {
-//   const stack = [tree[0]]
-//   while (stack.length) {
-//     const node = stack['shift']()
-//     if (node['id'] === value) return node
-//     node.children && stack.push(...node.children)
-//   }
-//   return null
-// }
-
-// const getComponent = (componentId) => {
-//   console.log(all.filter((c) => c.id === componentId))
-//   return all.filter((c) => c.id === componentId)
-//   //TODO: treesearch
-// }
-
 const clearStorage = () => localStorage.clear()
-
-//TODO: Read localStorage and load
 
 // Attach Events
 get('#clearStorage').addEventListener('click', clearStorage)
+
+//TODO: Read localStorage and load
